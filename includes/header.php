1bf66e66dc9c236c8a8c09dd328e5b41ce2beb97
Levi-Elias <le.sastro@outlook.com>
2025-06-27 14:55:13 +0200

    yes

--- includes/header.php
+++ includes/header.php
@@ -4,9 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/style.css?v=<?php echo filemtime('assets/style.css'); ?>">
 </head>
-<body>
     
-</body>
-</html>
---
+++ includes/nav.php
@@ -0,0 +1,20 @@
+<div class="demo-warning">
+    Let op: Dit is een demo-site!
+</div>
+<nav class="navbar custom-navbar">
+    <div class="custom-navbar-container">
+        <a class="navbar-brand" href="#">
+            <img src="assets/photos/logo.png" alt="Logo" class="navbar-logo">
+        </a>
+        <input type="checkbox" id="nav-toggle" class="nav-toggle">
+        <label for="nav-toggle" class="nav-toggle-label">
+            <span></span>
+        </label>
+        <ul class="navbar-nav">
+            <li><a href="#" class="nav-link active">Home</a></li>
+            <li><a href="#" class="nav-link">Diensten</a></li>
+            <li><a href="#" class="nav-link">Contact</a></li>
+            <li><a href="#" class="nav-link">Over ons</a></li>
+        </ul>
+    </div>
+</nav>
